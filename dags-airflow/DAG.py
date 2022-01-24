55cb2753c36bc06dda3906bf0d6fda4ee5aed626
Sixte de Maupeou <sixte@panakeia.ai>
2022-01-24 22:26:54 +0100

    first runnin version

--- dags-airflow/DAG.py
+++ dags-airflow/DAG.py
@@ -1,34 +1,37 @@
 from airflow.models import DAG
 from airflow.operators.python import PythonOperator
-from airflow.operators.bash import BashOperator
 from airflow.utils.dates import days_ago
 from datetime import timedelta
+import subprocess
 
-from tasks import fetch_filter_store_datasets
+from tasks import extract_top_datasets, fetch_filter_store_datasets
+
+minio_container_ip = '172.30.0.3'# subprocess.run(["docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' minio"], stdout=subprocess.PIPE).stdout
+mongo_container_ip = '172.30.0.4'# subprocess.run(["docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' mongo"], stdout=subprocess.PIPE).stdout
 
 with DAG(
     dag_id='dag_tp',
     schedule_interval='0 17 * * WED',
     start_date=days_ago(1),
     dagrun_timeout=timedelta(minutes=60),
-    tags=['example', 'python', 'bash'],
-    params={"example_key": "example_value"},
+    tags=['python', 'minio', 'mongo'],
 ) as dag:
     dag.doc_md = __doc__
     dag.doc_md = """
     This is a DAG built for the data engineering TP
     """
-
-    fetch_task = PythonOperator(
-        task_id="fetch-datasets", 
-        python_callable=fetch_filter_store_datasets
+    data_gouv_to_s3_task = PythonOperator(
+        task_id="datasets-to-s3", 
+        python_callable=fetch_filter_store_datasets,
+        op_kwargs={"minio_container_ip": minio_container_ip},
     )
 
-    test_bash = BashOperator(
-        task_id='test-bash',
-        bash_command='echo 1',
+    s3_to_mongo_task = PythonOperator(
+        task_id='dataset-to-mongo',
+        python_callable=extract_top_datasets,
+        op_kwargs={"minio_container_ip": minio_container_ip, "mongo_container_ip": mongo_container_ip},
     )
 
 
-    fetch_task >> test_bash
+    data_gouv_to_s3_task >> s3_to_mongo_task
 
--- dags-airflow/tasks.py
+++ dags-airflow/tasks.py
@@ -1,32 +1,85 @@
 import boto3
 from datetime import datetime
-import pandas as pd
 from io import StringIO
-import requests
+import pandas as pd
+from pymongo import MongoClient
 
 
 def filter_datasets(df: pd.DataFrame) -> pd.DataFrame:
     """Filter to keep only datasets where the terms mobilité or transport are present."""
-    return df[df.title.str.lower().contains('mobilité|transport', regex=True)
-                | df.tags.str.lower().contains('mobilité|transport', regex=True)
-                | df.description.str.lower().contains('mobilité|transport', regex=True)]
+    return df[df.title.str.lower().str.contains('mobilité|transport', regex=True)
+                | df.tags.str.lower().str.contains('mobilité|transport', regex=True)
+                | df.description.str.lower().str.contains('mobilité|transport', regex=True)]
 
 
-def store_datasets(datasets_df: pd.DataFrame, bucket = 'datagouv') -> None:
-    """Store datasets in a Minio bucket in an object named with the current date (YYYY-MM-DD)"""
-    s3 = boto3.resource('s3', 
-        endpoint_url='http://0.0.0.0:9000', 
-        config=boto3.session.Config(signature_version='s3v4')
+def get_s3_resource(minio_container_ip: str):
+    """Utility function to get an s3 resource from Minio"""
+    return boto3.resource('s3', 
+        endpoint_url=f'http://{minio_container_ip}:9000', 
+        config=boto3.session.Config(signature_version='s3v4'),
+        aws_access_key_id='minioadmin',
+        aws_secret_access_key='miniopassword'
     )
+
+def get_boto3_client(minio_container_ip: str):
+    """Utility function to get an s3 client connecting to Minio"""
+    return boto3.client(
+        's3',
+        aws_access_key_id='minioadmin',
+        aws_secret_access_key='miniopassword',
+        endpoint_url=f'http://{minio_container_ip}:9000'
+    )
+
+def store_datasets(datasets_df: pd.DataFrame, minio_container_ip:str, bucket_name: str = 'datagouv') -> None:
+    """Store datasets in a Minio bucket in an object named with the current date (YYYY-MM-DD)"""
+    print(minio_container_ip)
+    s3 = get_s3_resource(minio_container_ip)
     csv_buffer = StringIO()
     datasets_df.to_csv(csv_buffer)
     current_date = datetime.today().strftime('%Y-%m-%d')
-    s3.Object(bucket, current_date).put(Body=csv_buffer.getvalue())
+    if bucket_name not in [bucket.name for bucket in s3.buckets.all()]:
+        s3.create_bucket(Bucket=bucket_name)
+    s3.Object(bucket_name, f'{current_date}/df.csv').put(Body=csv_buffer.getvalue())
+
+
+def load_datasets(minio_container_ip: str, bucket_name = 'datagouv') -> pd.DataFrame:
+    s3_client = get_boto3_client(minio_container_ip)
+    # TODO: Make current date a param of the DAG
+    current_date = datetime.today().strftime('%Y-%m-%d')
+    obj = s3_client.get_object(Bucket=bucket_name, Key=f'{current_date}/df.csv')
+    return pd.read_csv(obj['Body'])
 
 
 
-def fetch_filter_store_datasets() -> None:
-    stable_datasets_url = "https://www.data.gouv.fr/fr/datasets/r/f868cca6-8da1-4369-a78d-47463f19a9a3"
-    datasets_df = pd.read_csv(stable_datasets_url, sep=";")
+def fetch_filter_store_datasets(**kwargs) -> None:
+    stable_datasets_url = 'https://www.data.gouv.fr/fr/datasets/r/f868cca6-8da1-4369-a78d-47463f19a9a3'
+    datasets_df = pd.read_csv(stable_datasets_url, sep=';')
+    print(len(datasets_df))
     filtered_datasets_df = filter_datasets(datasets_df)
-    store_datasets(filtered_datasets_df)
+    print(len(filtered_datasets_df))
+    store_datasets(filtered_datasets_df, kwargs['minio_container_ip'])
+
+
+def get_top_orgs(n_orgs: int=30) -> pd.DataFrame:
+    orgs_csv_url = 'https://www.data.gouv.fr/fr/datasets/r/b7bbfedc-2448-4135-a6c7-104548d396e7'
+    orgs_df = pd.read_csv(orgs_csv_url, sep=';')
+    top_orgs_df = orgs_df.sort_values(by='metric.followers', ascending=False)[:n_orgs]
+    return top_orgs_df
+
+
+def create_mongo_document(datasets_df: pd.DataFrame, mongo_container_ip: str) -> None:
+    client = MongoClient(mongo_container_ip, 27017)
+    tops_collection = client.datagouv.tops
+    tops_collection.insert_one({"top_30_orgs_datasets": datasets_df.to_dict('records')})
+
+
+def extract_top_datasets(**kwargs) -> None:
+    """Fetch datasets from S3, filter to keep only top orgs and store in Mongo"""
+    datasets_df = load_datasets(kwargs['minio_container_ip'])
+    top_orgs_df = get_top_orgs()
+
+    filtered_datasets_df = pd.merge(datasets_df, top_orgs_df, left_on="organization_id", right_on="id", how='inner')
+    filtered_datasets_df = filtered_datasets_df[['id_x', 'title', 'metric.followers_x', 'organization_id', 'metric.followers_y']]
+    filtered_datasets_df = filtered_datasets_df.rename(columns={"id_x": "id", "metric.followers_x": "metric.followers", "metric.followers_y": "metric.organization_followers"})
+
+    create_mongo_document(filtered_datasets_df, kwargs['mongo_container_ip'])
